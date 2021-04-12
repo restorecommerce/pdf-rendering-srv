d6b1c15500c46e7fcd2d6dfd9e9f26f9c23a6747
Radu1990 <et2323@gmail.com>
2021-04-12 10:06:55 +0200

    add gitlab actions, remove old dockerfile

--- buildImage.bash
+++ buildImage.bash
@@ -4,7 +4,7 @@ SERVICE_NAME="pdf-rendering-srv"
 
 DOCKER_BUILDKIT=1 docker build \
   --tag restorecommerce/$SERVICE_NAME \
-  -f ./buildkit.Dockerfile \
+  -f ./Dockerfile \
   --cache-from restorecommerce/$SERVICE_NAME \
   --build-arg APP_HOME=/home/node/$SERVICE_NAME \
   .
